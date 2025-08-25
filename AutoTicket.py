4db752d617a1793fdd1c91486d9230ed81908208
BAOfanTing <1316149505@qq.com>
2025-08-25 16:54:56 +0800

    更新文档

--- AutoTicket.py
+++ AutoTicket.py
@@ -20,11 +20,11 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 # =========================参数配置======= = ==========================
 CHANNEL = "02"
 APP_VER_NO = "3.1.4"
-SES_ID = "bc18314fa23243caba29427098662d08" # 重新登录后会变
+SES_ID = "95616c4807af45c4a5205b33a3d64acb" # 重新登录后会变
 LOGIN_NAME_PLAINTEXT = "HFbSkQ7f/BeguGThXNyVwQ=="
 USER_ID_PLAINTEXT = "HFbSkQ7f/BeguGThXNyVwQ=="
 EXCHANGE_ID_PLAINTEXT = "10"   #9是2块,10是4块,11是6块
-RUN_TIME = datetime(2025, 8, 19, 17, 00, 1, 500000)  # 2025-08-16 06:59:59.900
+RUN_TIME = datetime(2025, 8, 25, 17, 00, 1, 500000)  # 2025-08-16 06:59:59.900
 RUN_COUNT = 50                # 运行次数
 
 # ======================================= = ==========================
